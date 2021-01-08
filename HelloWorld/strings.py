07f1ff30f279c753f33e5a46532a077a2bc44df0
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-08 15:55:30 +0000

    introduction to strings in Python

---
+++ HelloWorld/strings.py
@@ -0,0 +1,5 @@
+print('Today is a good day to learnm python');
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
