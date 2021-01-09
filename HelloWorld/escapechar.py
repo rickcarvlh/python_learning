7d74b72035ad34405b2a3a9a2eabe146e2b23f42
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-09 15:17:37 +0000

    escape character examples

---
+++ HelloWorld/escapechar.py
@@ -0,0 +1,24 @@
+splitstring = 'This string has been\nsplit over\nserveral\nlines'
+print(splitstring)
+
+# tab
+tabbedString = '1\t2\t3\t4\t5'
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+# or
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+anotherSpliString = """This string has been
+split over
+several
+lines"""
+print(anotherSpliString)
+
+escapeEndOfLine = """This string has not been \
+split over \
+several \
+lines"""
+print(escapeEndOfLine)
