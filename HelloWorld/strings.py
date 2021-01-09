6067fa4ebd09232301b1b540f678bf1006864e2e
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-09 14:57:40 +0000

    strings part 2

--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -3,3 +3,8 @@ print('Python is fun')
 print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
+greeting = 'Hello'
+name = input('Please enter your name ')
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
