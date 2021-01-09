da1e9b4bc5aa03e903596ce08d005daffe7cb6ac
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-09 15:45:38 +0000

    variables and types

--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -4,7 +4,17 @@ print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = 'Hello'
-name = input('Please enter your name ')
+# name = input('Please enter your name ')
+nameVariable = 'Ricardo'
+
 
 # if we want a space, we can add that too
-print(greeting + ' ' + name)
+#print(greeting + ' ' + name)
+
+print(greeting + ' ' + nameVariable)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
