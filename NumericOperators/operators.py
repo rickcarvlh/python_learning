2fa6a72ae6fe8be1f24b1ff747ac3bdd05359a3b
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-10 15:20:07 +0000

    operators

---
+++ NumericOperators/operators.py
@@ -0,0 +1,15 @@
+a = 12
+b = 3
+
+print(a + b) # 15
+print(a - b) # 9
+print(a * b) # 36
+print(a / b) # 4.0
+print(a // b) # 4 integer division, rounded towards infinity
+print(a % b) # 0 modulo: the remainder after integer division
+
+print()
+
+# must be integers
+for i in range(1, 4):
+    print(i)
