b1400f8100f939d157355d67d2ed67a25cc56887
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-11 18:56:08 +0000

    exercise solution

--- HelloWorld/sliceback.py
+++ HelloWorld/sliceback.py
@@ -2,3 +2,12 @@ letters = "abcdefghijklmnopqrstuvwxyz"
 
 backwards = letters[::-1]
 print(backwards)
+
+# create a slice that produces the characters qpo
+print(letters[16:13:-1])
+
+# slice the string to produce edcba
+print(letters[4::-1])
+
+# slice the string to produce the last 8 characters, in reverse order
+print(letters[:-9:-1])
