b872b802492652f6c1f5ded05a112f27e3320393
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-11 18:20:39 +0000

    slicing

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -19,3 +19,10 @@ print(parrot[-11])
 print(parrot[-8])
 print(parrot[-6])
 
+print(parrot[0:6]) #Norweg
+print(parrot[0:9])
+print(parrot[:9])
+print(parrot[10:14])
+print(parrot[:6] + parrot[6:])
+print(parrot[:])
+
