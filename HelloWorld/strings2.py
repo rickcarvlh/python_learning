1f2b8ff944bf2ecc92260d51555b9c09f3b0879a
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-11 18:05:07 +0000

    negative indexes

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -1,6 +1,6 @@
-#         012345678901234
+#         01234567890123
 parrot = 'Norwegian Blue'
-
+         
 print(parrot)
 
 print(parrot[3])
@@ -9,3 +9,13 @@ print()
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
+
+print()
+
+print(parrot[-11])
+print(parrot[-10])
+print()
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
