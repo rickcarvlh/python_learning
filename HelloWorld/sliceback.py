441e29ed3dc884da14de0217dea383ee74cf6743
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-01-28 18:21:38 +0000

    string operators

--- HelloWorld/sliceback.py
+++ HelloWorld/sliceback.py
@@ -11,3 +11,9 @@ print(letters[4::-1])
 
 # slice the string to produce the last 8 characters, in reverse order
 print(letters[:-9:-1])
+
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
