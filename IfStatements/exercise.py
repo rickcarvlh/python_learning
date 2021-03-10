bb895c3dd7026e54fa453e2ce893fb98da9cf0ab
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2021-03-10 18:14:38 +0000

    exercises done

---
+++ IfStatements/exercise.py
@@ -0,0 +1,7 @@
+tree1 = 'put your first tree name here'
+tree2 = 'put your second tree name here'
+ 
+if tree1 == tree2:
+    print("The trees are the same")
+else:
+    print("The trees are different")
---
+++ IfStatements/simpleCondition.py
@@ -0,0 +1,10 @@
+x = 5
+y = 7
+
+if x > y:
+    print("x i greater than y")
+elif x < y:
+    print("x is smaller than y")
+else:
+    print("x equals y")
+    
